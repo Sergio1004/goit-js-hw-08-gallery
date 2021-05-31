2334a784fb178bf1383773ef84f366e8ad4b987e
Serhii Krimets <krimets.s88@gmail.com>
2021-05-31 05:50:27 +0300

    add modal image

--- gallery.js
+++ gallery.js
@@ -14,8 +14,49 @@ const createGalleryItemList = (array) =>
         <li class="gallery__item">
             <a class="gallery__link" href = '${original}'>
                 <img class="gallery__image" src ='${preview}' data-source = '${original}' alt = '${description}'/>
-            <a/>
+            </a>
         </li>`
     }).join('');
 
-refs.gallery.insertAdjacentHTML('afterbegin', createGalleryItemList(galleryImages));
+refs.gallery.insertAdjacentHTML ('afterbegin', createGalleryItemList(galleryImages));
+
+const onOpenModal = (e) => {
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    };
+    e.preventDefault();
+
+    refs.modal.classList.add('is-open');
+    const currentImageRef = e.target.dataset.source;
+    const currentAltRef = e.target.alt;
+    
+    refs.modalImage.src = `${currentImageRef}`;
+    refs.modalImage.alt = `${currentAltRef}`;
+
+    window.addEventListener('keydown', onEscapePress)
+}
+
+refs.gallery.addEventListener('click', onOpenModal);
+
+const onCloseModal = () => {
+    refs.modal.classList.remove('is-open');
+    refs.modalImage.src = '';
+    refs.modalImage.alt = '';
+    window.removeEventListener('keydown', onEscapePress)
+};
+
+refs.closeModal.addEventListener('click', onCloseModal);
+
+const onOverlayClick = (e) => {
+    if (e.target === refs.modalOverlay) {
+        onCloseModal();
+    }
+}
+
+refs.modalOverlay.addEventListener('click', onOverlayClick);
+
+const onEscapePress = (e) => {
+    if (e.code === 'Escape') {
+        onCloseModal();
+    }
+}
